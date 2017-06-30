d9023d336da6df91fa19f2ec8f77112e8f4b7b34
Charlie Weems <charlie.weems@gmail.com>
2017-06-29 17:31:20 -0700

    Initial cookie implementation

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('script')
+$(document).ready(function(){
+  $(".checkbox input").each(function() {
+    var cookie = $.cookie($(this).attr('name'));
+    console.log('fire')
+    if (cookie && cookie == "true") {
+        $(this).prop('checked', cookie);
+    }
+  });
+
+  $(".checkbox input").change(function() {
+    $.cookie($(this).attr("name"), $(this).prop('checked'), {
+        path: '/',
+        expires: 365
+    });
+  });
+});
